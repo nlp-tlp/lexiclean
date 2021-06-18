1f5ff136d551f603fb068a124b0ef075d5e3da73
4theKnowledge <tylerbikaun@gmail.com>
2021-06-18 09:44:15 +0800

    reformatted

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -2,16 +2,26 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { useHistory } from 'react-router-dom'
 import { createUseStyles } from 'react-jss';
-import { Dropdown } from 'react-bootstrap';
+import { Dropdown, Container, Row, Col, Spinner } from 'react-bootstrap';
 import { MdSave } from 'react-icons/md'
 
 const useStyles = createUseStyles({
+    // header: {
+    //     paddingTop: '0.5em',
+    //     paddingBottom: '0.5em',
+    //     backgroundColor: '#8F8F8F',
+    //     maxWidth: '100%',
+    //     display: 'flex',
+    //     justifyContent: 'space-between',
+    //     borderBottom: '2px #D9D9D9 solid',
+    //     position: 'sticky',
+    //     top: '0',
+    // },
     header: {
         paddingTop: '0.5em',
         paddingBottom: '0.5em',
         backgroundColor: '#8F8F8F',
         maxWidth: '100%',
-        display: 'flex',
         justifyContent: 'space-between',
         borderBottom: '2px #D9D9D9 solid',
         position: 'sticky',
@@ -91,73 +101,89 @@ export default function Header({project,
     }, [project, pageChanged])
 
 
-
-
-
-
-
-
-
-
     return (
-        <div className={classes.header}>
-                <div className={classes.title}>
-                    Lexiclean
-                </div>
-                <div
-                    style={{display: 'flex', flexDirection: 'column'}}
-                >
-                    <div
-                        style={{fontSize: '2em', color: '#F8F9FA', fontWeight: 'bolder', display: 'flex', justifyContent: 'center'}}
-                    >
-                        <p>
-                            { project.name }
-                        </p>
-                        <p
-                            className={classes.save}
-                            onClick={() => setSaved(true)}
-                            title={`${changeCount} changes waiting`}
-                        >
-                            <MdSave/>
-                        </p>
+        <Container className={classes.header}>
+            <Row className="justify-content-md-center">
+                <Col md="2" style={{textAlign: 'center'}}>
+                    <p className={classes.title}>Lexiclean</p>
+                </Col>
+                <Col md="8">
+                    <p className="text-center" style={{fontSize: '2em', color: '#F8F9FA', fontWeight: 'bolder'}}>
+                        {project.name}
+                    </p>
+                </Col>
+                <Col md="2">
+                    <div className={classes.menu}>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="light" id="dropdown-basic">
+                                Menu
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                {/* <Dropdown.Item onClick={() => setShowOverview(true)}>Overview</Dropdown.Item> */}
+                                <Dropdown.Item onClick={() => setShowLegend(true)}>Legend</Dropdown.Item>
+                                <Dropdown.Item onClick={() => setShowDownload(true)}>Download Results</Dropdown.Item>
+                                <Dropdown.Item onClick={() => setShowProgress(true)}>Review Progress</Dropdown.Item>
+                                <Dropdown.Item onClick={() => setShowModifySchema(true)}>Modify Schema</Dropdown.Item>
+                                <Dropdown.Item onClick={() => setShowSettings(true)}>Settings</Dropdown.Item>
+                                <Dropdown.Item onClick={() => history.push('/')}>Home</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
                     </div>
-                    {
-                        project.metrics && progress ?
-                        <div
-                            style={{fontSize: '1em', color: '#F8F9FA', textAlign: 'center'}}
-                        >
-                            <div>
-                                <strong>Documents Annotated:</strong> {' '}
-                                {progress.annotated} / {progress.total}
-                            </div>
-                            <div>
-                                <strong>Token Reduction:</strong> {' '} {Math.round((1-(currentVocabSize/project.metrics.starting_vocab_size)) * 100)}%
-                            </div>
-                            <div>
-                                <strong>OOV Corrected:</strong> {' '} {project.metrics.starting_oov_token_count - currentOOVTokenCount} / {project.metrics.starting_oov_token_count}
-                            </div>
-                        </div>
-                        : null
-                    }
-                </div>
-                <div className={classes.menu}>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="light" id="dropdown-basic">
-                            Menu
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            {/* <Dropdown.Item onClick={() => setShowOverview(true)}>Overview</Dropdown.Item> */}
-                            <Dropdown.Item onClick={() => setShowLegend(true)}>Legend</Dropdown.Item>
-                            <Dropdown.Item onClick={() => setShowDownload(true)}>Download Results</Dropdown.Item>
-                            <Dropdown.Item onClick={() => setShowProgress(true)}>Review Progress</Dropdown.Item>
-                            <Dropdown.Item onClick={() => setShowSettings(true)}>Settings</Dropdown.Item>
-                            <Dropdown.Item onClick={() => setShowModifySchema(true)}>Modify Schema</Dropdown.Item>
-                            <Dropdown.Item onClick={() => history.push('/')}>Home</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col md="3"></Col>
+            {
+                progress && project && currentVocabSize ?
+                    <Col md="6" className="text-center">
+                        <Row>
+                            <Col>
+                                <div style={{display: 'flex', flexDirection: 'column', padding: '0em', width: '100%'}}>
+                                    <p style={{margin: '0em', fontSize: '1.5em', fontWeight: 'bolder'}}>
+                                        {progress.annotated} / {progress.total}
+                                    </p>
+                                    <p style={{fontSize: '0.75em', fontWeight: 'bold'}}>
+                                        Docs Annotated
+                                    </p>
+                                </div>
+                            </Col>
+                            <Col>
+                                <div style={{display: 'flex', flexDirection: 'column', padding: '0em', width: '100%'}}>
+                                    <p style={{margin: '0em', fontSize: '1.5em', fontWeight: 'bolder'}}>
+                                        {Math.round((1-(currentVocabSize/project.metrics.starting_vocab_size)) * 100)}%
+                                    </p>
+                                    <p style={{fontSize: '0.75em', fontWeight: 'bold'}}>
+                                        Token Reduction
+                                    </p>
+                                </div>
+                            </Col>
+                            <Col>
+                                <div style={{display: 'flex', flexDirection: 'column', padding: '0em', width: '100%'}}>
+                                    <p style={{margin: '0em', fontSize: '1.5em', fontWeight: 'bolder'}}>
+                                        {project.metrics.starting_oov_token_count - currentOOVTokenCount} / {project.metrics.starting_oov_token_count}
+                                    </p>
+                                    <p style={{fontSize: '0.75em', fontWeight: 'bold'}}>
+                                        OOV Corrected
+                                    </p>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Col>
+                :
+                <Col md="6" className="text-center"><Spinner animation="border" size="sm" variant="light"/></Col>
+            }
+                <Col md="3"></Col>
+            </Row>
 
 
-        </div>
+
+        </Container>
     )
 }
+// <p
+// className={classes.save}
+// onClick={() => setSaved(true)}
+// title={`${changeCount} changes waiting`}
+// >
+// <MdSave/>
+// </p>
