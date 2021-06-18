1c3abc61d7827af88948a2a9aa9df99e856be7e7
4theKnowledge <tylerbikaun@gmail.com>
2021-06-18 09:59:19 +0800

    more formatting

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -2,28 +2,12 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { useHistory } from 'react-router-dom'
 import { createUseStyles } from 'react-jss';
-import { Dropdown, Container, Row, Col, Spinner } from 'react-bootstrap';
+import { Dropdown, Container, Row, Col, Spinner, Card } from 'react-bootstrap';
 import { MdSave } from 'react-icons/md'
 
 const useStyles = createUseStyles({
-    // header: {
-    //     paddingTop: '0.5em',
-    //     paddingBottom: '0.5em',
-    //     backgroundColor: '#8F8F8F',
-    //     maxWidth: '100%',
-    //     display: 'flex',
-    //     justifyContent: 'space-between',
-    //     borderBottom: '2px #D9D9D9 solid',
-    //     position: 'sticky',
-    //     top: '0',
-    // },
     header: {
-        paddingTop: '0.5em',
-        paddingBottom: '0.5em',
-        backgroundColor: '#8F8F8F',
         maxWidth: '100%',
-        justifyContent: 'space-between',
-        borderBottom: '2px #D9D9D9 solid',
         position: 'sticky',
         top: '0',
     },
@@ -52,7 +36,7 @@ const useStyles = createUseStyles({
     save: {
         marginLeft: '0.25em',
         fontSize: '36px',
-        color: 'green',
+        color: 'grey',
         cursor: 'pointer',
     },
 })
@@ -103,7 +87,10 @@ export default function Header({project,
 
     return (
         <Container className={classes.header}>
-            <Row className="justify-content-md-center">
+            <Row
+                className="justify-content-md-center"
+                style={{ paddingTop: '0.5em', paddingBottom: '0.5em', backgroundColor: '#8F8F8F', borderBottom: '2px #D9D9D9 solid' }}
+            >
                 <Col md="2" style={{textAlign: 'center'}}>
                     <p className={classes.title}>Lexiclean</p>
                 </Col>
@@ -122,7 +109,7 @@ export default function Header({project,
                                 {/* <Dropdown.Item onClick={() => setShowOverview(true)}>Overview</Dropdown.Item> */}
                                 <Dropdown.Item onClick={() => setShowLegend(true)}>Legend</Dropdown.Item>
                                 <Dropdown.Item onClick={() => setShowDownload(true)}>Download Results</Dropdown.Item>
-                                <Dropdown.Item onClick={() => setShowProgress(true)}>Review Progress</Dropdown.Item>
+                                {/* <Dropdown.Item onClick={() => setShowProgress(true)}>Review Progress</Dropdown.Item> */}
                                 <Dropdown.Item onClick={() => setShowModifySchema(true)}>Modify Schema</Dropdown.Item>
                                 <Dropdown.Item onClick={() => setShowSettings(true)}>Settings</Dropdown.Item>
                                 <Dropdown.Item onClick={() => history.push('/')}>Home</Dropdown.Item>
@@ -131,19 +118,29 @@ export default function Header({project,
                     </div>
                 </Col>
             </Row>
-            <Row>
-                <Col md="3"></Col>
+            <Row
+                style={{backgroundColor: 'white', opacity: '0.9'}}
+            >
+                <Col md="3" className="text-left">
+                    <p
+                        className={classes.save}
+                        onClick={() => setSaved(true)}
+                        title={`${changeCount} changes waiting`}
+                    >
+                        <MdSave/>
+                    </p>
+                </Col>
             {
                 progress && project && currentVocabSize ?
                     <Col md="6" className="text-center">
-                        <Row>
+                        <Row style={{marginTop: '0.5em', backgroundColor: 'white', opacity: '1'}}>
                             <Col>
                                 <div style={{display: 'flex', flexDirection: 'column', padding: '0em', width: '100%'}}>
                                     <p style={{margin: '0em', fontSize: '1.5em', fontWeight: 'bolder'}}>
-                                        {progress.annotated} / {progress.total}
+                                    {progress.annotated} / {progress.total}
                                     </p>
                                     <p style={{fontSize: '0.75em', fontWeight: 'bold'}}>
-                                        Docs Annotated
+                                    Docs Annotated
                                     </p>
                                 </div>
                             </Col>
@@ -180,10 +177,3 @@ export default function Header({project,
         </Container>
     )
 }
-// <p
-// className={classes.save}
-// onClick={() => setSaved(true)}
-// title={`${changeCount} changes waiting`}
-// >
-// <MdSave/>
-// </p>
