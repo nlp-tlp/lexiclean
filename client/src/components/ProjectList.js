8cad46d6d5abe028a3fc94e62801c9b3650a7e94
4theKnowledge <tylerbikaun@gmail.com>
2021-06-21 07:41:23 +0800

    little bit of styling

--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { ListGroup } from 'react-bootstrap';
 import { MdDelete, MdEdit, MdFileDownload } from 'react-icons/md';
+import { RiNumbersFill } from 'react-icons/ri'
 
 import DownloadModal from './modals/DownloadModal';
 
@@ -48,24 +49,58 @@ export default function ProjectList({projects, setSelectedProject, setShowAnnota
                                 style={{display: 'flex', justifyContent: 'space-between'}}
                                 key={index}
                             >
-                                <div 
-                                    style={{fontSize: '22px', fontWeight: 'bold'}}
+                                <div
                                     onClick={() => confirmationAction(index)}
                                     key={index}
                                 >
-                                    {project.name}
+                                    <h1 style={{fontSize: '32px', fontWeight: 'bold', margin: 'auto'}} >
+                                        {project.name}
+                                    </h1>
+                                    <p style={{fontSize: '20px', margin: 'auto', verticalAlign: 'middle'}} >
+                                        {project.description}
+                                    </p>
                                     <br/>
-                                    <small>{project.description}</small>
-                                    <br/>
-                                    <small>{project.annotated_texts}/{project.text_count}</small>
-
+                                    <p style={{fontSize: '12px', margin: 'auto'}}>{project.created_on}</p>
                                 </div>
-                                <div style={{fontSize: '22px'}}>
-                                    <MdEdit onClick={() => confirmationAction(index)}/>
-                                    <MdFileDownload onClick={() => downloadHandler(project)}/> 
-                                    <MdDelete style={{color: '#D95F69'}} onClick={() => deleteAction(index)}/>
-                                    <br/>
-                                    <p style={{fontSize: '12px'}}>{project.created_on}</p>
+                                <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                                    <div style={{margin: '0em 2em 0em 2em'}}>
+                                        <div style={{width: '36px', height: '36px', fontSize: '22px', textAlign: 'center', borderRadius: '50%', margin: 'auto', color: 'white', paddingRight: '0.1em', backgroundColor: 'grey'}}>
+                                            <RiNumbersFill/>
+                                        </div>
+                                        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', paddingLeft: '0.5em', padding: '0em', margin: 'auto', textAlign: 'center'}}>
+                                            <div>
+                                                <p style={{fontWeight: 'bolder', fontSize: '18px', margin: '0'}}>{project.annotated_texts}/{project.text_count}</p>
+                                                <p style={{fontSize: '14px'}}>Complete</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div style={{margin: '0em 2em 0em 2em', color: 'lightgrey'}}>
+                                        <div style={{width: '36px', height: '36px', fontSize: '22px', textAlign: 'center', borderRadius: '50%', margin: 'auto', color: 'white', paddingRight: '0.1em', backgroundColor: 'lightgrey'}}>
+                                            <RiNumbersFill/>
+                                        </div>
+                                        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', paddingLeft: '0.5em', padding: '0em', margin: 'auto', textAlign: 'center'}}>
+                                            <div>
+                                                <p style={{fontWeight: 'bolder', fontSize: '18px', margin: '0'}}>{project.annotated_texts}/{project.text_count}</p>
+                                                <p style={{fontSize: '14px'}}>Complete</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div style={{margin: '0em 2em 0em 2em', color: 'lightgrey'}}>
+                                        <div style={{width: '36px', height: '36px', fontSize: '22px', textAlign: 'center', borderRadius: '50%', margin: 'auto', color: 'white', paddingRight: '0.1em', backgroundColor: 'lightgrey'}}>
+                                            <RiNumbersFill/>
+                                        </div>
+                                        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', paddingLeft: '0.5em', padding: '0em', margin: 'auto', textAlign: 'center'}}>
+                                            <div>
+                                                <p style={{fontWeight: 'bolder', fontSize: '18px', margin: '0'}}>{project.annotated_texts}/{project.text_count}</p>
+                                                <p style={{fontSize: '14px'}}>Complete</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div style={{display: 'flex', flexDirection: 'column', fontSize: '22px'}}>
+                                    <MdEdit style={{margin: '0em 0.5em 0.5em 0em'}} onClick={() => confirmationAction(index)}/>
+                                    <MdFileDownload style={{margin: '0em 0.5em 0.5em 0em'}} onClick={() => downloadHandler(project)}/> 
+                                    <MdDelete style={{color: '#D95F69', margin: '0em 0.5em 0.5em 0em'}} onClick={() => deleteAction(index)}/>
                                 </div>
                             </div>
                         </ListGroup.Item>
