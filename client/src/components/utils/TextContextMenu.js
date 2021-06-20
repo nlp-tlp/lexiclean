0c4c7d467f0eaedea7eba800a50429a3f8efe457
4theKnowledge <tylerbikaun@gmail.com>
2021-06-21 06:30:04 +0800

    added undo and disabled apply all

--- client/src/components/utils/TextContextMenu.js
+++ client/src/components/utils/TextContextMenu.js
@@ -2,29 +2,27 @@ import React from 'react'
 import { Menu, Item } from "react-contexify";
 import "react-contexify/dist/ReactContexify.css";
 
-export default function TextContextMenu({menu_id, textString, updateText}) {
+export default function TextContextMenu({menu_id, textString, setTextString, updateText, originalText}) {
 
     const Apply = (textString) => {
-        console.log('apply one')
         const isSingle = true;
         updateText(textString, isSingle)
     }
 
     const ApplyAll = (textString) => {
         console.log('apply all')
-        const isSingle = false;
+        // const isSingle = false;
     }
 
-    // const Undo = (field) => {
-    //     // removeMetaTag(field)
-    // }
-
+    const Undo = () => {
+        setTextString(originalText);
+    }
 
     return (
         <Menu id={menu_id}>
             <Item onClick={() => Apply(textString)}>Apply</Item>
-            <Item onClick={() => ApplyAll(textString)}>Apply All</Item>
-            {/* <Item onClick={() => Undo()}>Undo</Item> */}
+            <Item disabled onClick={() => ApplyAll(textString)}>Apply All</Item>
+            <Item onClick={() => Undo()}>Undo</Item>
         </Menu>
     )
 }
