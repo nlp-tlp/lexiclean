6d792e04ad659094649bc186c9f43a797f5da51d
4theKnowledge <tylerbikaun@gmail.com>
2021-06-23 08:15:12 +0800

    added directories to gitignore. added jwt validation into useToken hook

--- client/src/components/auth/useToken.js
+++ client/src/components/auth/useToken.js
@@ -1,16 +1,31 @@
 import { useState } from 'react'
+import axios from 'axios';
 
 export default function useToken() {
 
     const getToken = () => {
         const tokenString = localStorage.getItem('token');
         const userToken = JSON.parse(tokenString);
-        return userToken
+
+        // Check if token valid
+        axios({
+                method: 'post',
+                url: '/api/auth/token/validate',
+                data: {token: userToken}})
+                .then(function (response) {
+                  if (response.status === 200){
+                    if (response.data.valid){
+                      return userToken;
+                    } else if (!response.data.valid) {
+                      localStorage.removeItem('token');
+                    }
+                  }
+                })
       }
 
     const [token, setToken] = useState(getToken());
 
-    const saveToken = userToken => {
+    const saveToken = (userToken) => {
         localStorage.setItem('token', JSON.stringify(userToken));
         setToken(userToken);
       }
