d7220e0dd2b3f1ca75fdccc6a1d72c8da204232b
4theKnowledge <tylerbikaun@gmail.com>
2024-11-04 05:59:51 +0800

    fix: add navigation to base url on brand click.

--- client/src/shared/components/Layout/BrandToolbar.jsx
+++ client/src/shared/components/Layout/BrandToolbar.jsx
@@ -1,14 +1,28 @@
-import { Stack, Typography } from "@mui/material";
+import { Link, Stack, Typography } from "@mui/material";
 import BubbleChartIcon from "@mui/icons-material/BubbleChart";
 
 const BrandToolbar = () => {
   return (
-    <Stack direction="row" alignItems="center" spacing={2} ml={1}>
-      <BubbleChartIcon sx={{ color: "primary.main" }} />
-      <Typography fontWeight={500} fontSize={20} sx={{ color: "primary.main" }}>
-        LexiClean
-      </Typography>
-    </Stack>
+    <Link
+      href="/"
+      sx={{
+        textDecoration: "none",
+        "&:hover": {
+          opacity: 0.8,
+        },
+      }}
+    >
+      <Stack direction="row" alignItems="center" spacing={2} ml={1}>
+        <BubbleChartIcon sx={{ color: "primary.main" }} />
+        <Typography
+          fontWeight={500}
+          fontSize={20}
+          sx={{ color: "primary.main" }}
+        >
+          LexiClean
+        </Typography>
+      </Stack>
+    </Link>
   );
 };
 
